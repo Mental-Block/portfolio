6f74f7b8ed84ed7a141947ba12cc9111d82c409b
mental-block <aaron.tibben@gmail.com>
2020-06-10 20:48:57 -0400

    good

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
 
 if (process.env.NODE_ENV === "production") {

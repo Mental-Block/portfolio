6df2416436b4d921cc0424ef34f290072538f11b
mental-block <aaron.tibben@gmail.com>
2020-06-13 15:09:37 -0400

    hopefully fixed routing issue

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../", "frontend", "build")));
 
   app.get("/*", (req, res) => {
-    return res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../", "frontend", "build");
   });
 }
 

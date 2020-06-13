bb07b6b0361cb68bfb9ab36aed0ebb77610901f6
mental-block <aaron.tibben@gmail.com>
2020-06-12 21:09:58 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../", "frontend", "build")));
+  // app.use(express.static(path.join(__dirname, "../", "frontend", "build")));
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../", "frontend", "build"), (err) => {
       if (err) {

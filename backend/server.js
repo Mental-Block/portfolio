39695818f57f2b36021100ecba7ddeb45ce4c38e
mental-block <aaron.tibben@gmail.com>
2020-06-13 14:38:15 -0400

    hopefully fixed routing issue

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,9 @@ app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../", "frontend", "build")));
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(
-      path.join(__dirname, "../", "frontend", "build", "index.html"),
-      (err) => {
-        if (err) {
-          res.status(500).send(err);
-        }
-      }
+      path.join(__dirname, "../", "frontend", "build", "index.html")
     );
   });
 }

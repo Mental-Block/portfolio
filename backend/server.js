a614e3317ffa68de878e4508559effbde236dfc0
mental-block <aaron.tibben@gmail.com>
2020-06-12 21:23:02 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,14 @@ app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static(path.join(__dirname, "../", "frontend", "build")));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../", "frontend", "build"), (err) => {
-      if (err) {
-        res.status(500).send(err);
+    res.sendFile(
+      path.join(__dirname, "../", "frontend", "build", "index.html"),
+      (err) => {
+        if (err) {
+          res.status(500).send(err);
+        }
       }
-    });
+    );
   });
 }
 

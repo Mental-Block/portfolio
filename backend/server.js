311296fd3c2ce0c017676637dd65f4da047f63f6
mental-block <aaron.tibben@gmail.com>
2020-06-13 14:32:32 -0400

    made dev prod easier

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../", "frontend", "build")));
-  app.get("/*/build/", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(
       path.join(__dirname, "../", "frontend", "build", "index.html"),
       (err) => {

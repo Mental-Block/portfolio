2ce5ea0a02b65acd13eff7d3d19ae609e5755f57
mental-block <aaron.tibben@gmail.com>
2020-06-13 15:19:12 -0400

    hopefully fixed routing issue

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,13 @@ app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../", "frontend", "build")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../", "frontend", "build", "index.html")
-    );
-  });
+  (() => {
+    app.get("/*", (req, res) => {
+      res.sendFile(
+        path.join(__dirname, "../", "frontend", "build", "index.html")
+      );
+    });
+  })();
 }
 
 app.use("/contact", require("./routes/emailRoutes"));
